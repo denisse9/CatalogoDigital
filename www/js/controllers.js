7f7ba1eaaf514cfad1d124ea1a328dee3574bf6e
MSA <marcoagsa@gmail.com>
2017-02-09 00:54:49 +0000

    Mapas a funcionar e melhorias menu!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -193,10 +193,149 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('centrosDeColheitaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('centrosDeColheitaCtrl', ['$scope', '$stateParams', '$ionicModal', '$ionicActionSheet', '$timeout', '$http', '$log', '$state', '$location', '$ionicPopup', '$compile', '$ionicLoading', 'geolocationService', 'geofenceService',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicModal, $ionicActionSheet, $timeout, $http, $log, $state, $location, $ionicPopup, $compile, $ionicLoading, geolocationService, geofenceService ) {
+
+	$scope.latLang={
+		lat:'',
+		lang:'',
+		location:''
+	};
+	
+		 $ionicLoading.show({
+            template: 'Getting geofences from device...',
+            duration: 5000
+        });
+
+        $scope.geofences = [];
+
+        geofenceService.getAll().then(function (geofences) {
+            $ionicLoading.hide();
+            $scope.geofences = geofences;
+        }, function (reason) {
+            $ionicLoading.hide();
+            $log.log('An Error has occured', reason);
+        });
+		
+		
+        $scope.GetGeoLocation = function () {
+			
+            $log.log('Tracing current location...');
+            $ionicLoading.show({
+                template: 'Tracing current location...'
+            });
+            geolocationService.getCurrentPosition()
+                .then(function (position) {
+                    $log.log('Current location found');
+                    $log.log('Current location Latitude'+position.coords.latitude);
+                    $log.log('Current location Longitude'+position.coords.longitude);
+					
+                    $ionicLoading.hide();
+					$scope.latLang.lat=parseFloat(position.coords.latitude);
+					$scope.latLang.lang=parseFloat(position.coords.longitude);
+					var lat =$scope.latLang.lat;
+					var lang =$scope.latLang.lang; 
+					//You can hit request upto 2500 per day on free of cost. 
+					var mrgdata='http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lang+'&sensor=true'
+					$http.get(mrgdata)
+							.success(function (response) { 
+							/* console.log(response.results[0].formatted_address); */
+							$scope.latLang.location=response.results[0].formatted_address;
+							console.log("Your Current Location is : " +$scope.latLang.location)
+							
+							var myLatlng = new google.maps.LatLng(lat,lang);
+        
+						var mapOptions = {
+						  center: myLatlng,
+						  zoom: 16,
+						  mapTypeId: google.maps.MapTypeId.ROADMAP
+						};
+						var map = new google.maps.Map(document.getElementById("map"),
+							mapOptions);
+						
+					   
+						 var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+						var compiled = $compile(contentString)($scope);
+
+						var infowindow = new google.maps.InfoWindow({
+						 
+						});
+						infowindow.setContent($scope.latLang.location);
+						infowindow.open(map, marker);
+
+						var marker = new google.maps.Marker({
+						  position: myLatlng,
+						  map: map,
+						  title: 'Current Location'
+						});
+
+						google.maps.event.addListener(marker, 'click', function() {
+						  infowindow.open(map,marker);
+						 
+						});
+
+						$scope.map = map;
+									
+							   
+				}).error(function (data, status, headers, config) {
+					console.log("error");
+					
+					 if (status == 0)
+						showalert("Error", "Errro Occured from Server site!");
+					else
+						showalert("Error", data); 
+				  
+				});
+
+		}, function (reason) {
+			$log.log('Cannot obtain current location', reason);
+		   
+			$ionicLoading.show({
+				template: 'Cannot obtain current location',
+				duration: 1500
+			});
+		});
+     };
+	 
+	 //This is default set location before fetching current location///
+	 //***************Start********************************//
+	 if($scope.latLang.lat==''){
+			var myLatlng = new google.maps.LatLng(39.22443051,-8.69356781);
+        
+						var mapOptions = {
+						  center: myLatlng,
+						  zoom: 20,
+						  mapTypeId: google.maps.MapTypeId.ROADMAP
+						};
+						var map = new google.maps.Map(document.getElementById("map"),
+							mapOptions);
+						
+					   
+						var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+						var compiled = $compile(contentString)($scope);
+
+						var infowindow = new google.maps.InfoWindow({
+						 
+						});
+						infowindow.setContent($scope.latLang.location);
+						infowindow.open(map, marker);
+
+						var marker = new google.maps.Marker({
+						  position: myLatlng,
+						  map: map,
+						  title: 'Current Location'
+						});
+
+						google.maps.event.addListener(marker, 'click', function() {
+						  infowindow.open(map,marker);
+						 
+						});
+
+						$scope.map = map;
+	 }
+	 //***********************End**********************************///
 
 
 }])
